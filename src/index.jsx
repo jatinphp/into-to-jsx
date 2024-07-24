17b159b2c489d32f9464351c61aa46dfe2541dad
jatinphp <jatin.dhorajiya@gmail.com>
2024-07-24 19:14:52 +0530

    time slot practice

--- src/index.jsx
+++ src/index.jsx
@@ -8,10 +8,35 @@ import ReactDOM from "react-dom";
 
 const name = "Jatin Dhorajiya";
 const year = new Date().getFullYear();
+const customStyle = {
+    color:"red",
+    fontSize: "35px"
+}
+const currentTime = new Date().getHours();
+const customTimeStyle = {
+    color:"blue"
+}
+let greeting;
+if(currentTime < 12){
+    greeting = "Good Morning";
+    customTimeStyle.color = "red";
+}else if(currentTime < 18){
+    greeting = "Good afternoon";
+    customTimeStyle.color = "green";
+}else{
+    greeting = "Good night";
+}
+
 ReactDOM.render(<div>
-    <p>This might mean that you need to specify the --mirror option upon cloning as well to get the remotes to your local repo.</p>
+    <h1 style={customTimeStyle}>{greeting}</h1>
+    <h2 style={customStyle}>Hello World</h2>
+    <h1 className="heading" contentEditable="true" spellCheck="false">This might mean that you need to specify the --mirror option upon cloning as well to get the remotes to your local repo.</h1>
     <p>Alternatively you can include a remote setup script in your repo and reference it in the developer setup manual of your project.</p>
-    <p>created by {name}</p>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg" alt="alt" className="blog-img" />
+    <img src="https://www.eatingwell.com/thmb/088YHsNmHkUQ7iNGP4375MiAXOY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/article_7866255_foods-you-should-eat-every-week-to-lose-weight_-04-d58e9c481bce4a29b47295baade4072d.jpg" alt="alt" className="blog-img" />
+    <img src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg" alt="alt" className="blog-img" />
+    
+    <p>created by {name}</p> 
     <p>copyright {year}</p>
     </div>, document.getElementById("root"));
 
